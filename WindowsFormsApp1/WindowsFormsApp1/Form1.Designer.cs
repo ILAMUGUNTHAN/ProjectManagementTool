7dd28b376cdc7f423dcd55e6edc604c7e5eab43e
ILAMUGUNTHAN <ilamugunthan.n2020cse@sece.ac.in>
2024-03-21 12:21:37 +0530

    board view's image has been aligned

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -34,20 +34,18 @@
             // addProject1
             // 
             this.addProject1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(237)))));
-            this.addProject1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.addProject1.Location = new System.Drawing.Point(0, 0);
+            this.addProject1.Margin = new System.Windows.Forms.Padding(1);
             this.addProject1.Name = "addProject1";
-            this.addProject1.Size = new System.Drawing.Size(696, 550);
+            this.addProject1.Size = new System.Drawing.Size(464, 357);
             this.addProject1.TabIndex = 0;
-            this.addProject1.Load += new System.EventHandler(this.addProject1_Load);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(696, 550);
+            this.ClientSize = new System.Drawing.Size(1110, 618);
             this.Controls.Add(this.addProject1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
